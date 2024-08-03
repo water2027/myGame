69d59e4dacae5045c5850f83fcdb67af3f52d9d4
qs_theme <qstxdy@gmail.com>
2024-08-03 23:44:38 +0800

    修复了图形点击的残影

--- mathKiller/graphics.js
+++ mathKiller/graphics.js
@@ -3,8 +3,9 @@ var lost = 0;
 
 class Background {
     totalScore = 0;
-    
+    static totalGraphics = 0;
     game = null;
+    generateInterval = null;
     max = 10;
 
     constructor(width = window.innerWidth, height = window.innerHeight) {
@@ -16,7 +17,7 @@ class Background {
             let x = e.offsetX;
             let y = e.offsetY;
             graphics.forEach((item) => {
-                if (item.isShooted(x, y,this.ctx)) {
+                if (item.isShooted(x, y, this.ctx)) {
                     this.totalScore += item.score;
                     graphics.splice(graphics.indexOf(item), 1);
                 }
@@ -28,48 +29,44 @@ class Background {
         let x = Math.random() * this.canvas.width;
         let y = 0;
         let speedX = Math.random() * 10 - 5;
-        let speedY = Math.random() * 10;
+        let speedY = Math.abs(Math.random() * 10 - 5);
         let score = Math.floor(Math.random() * 10);
         let color = `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`;
         let width = Math.random() * 50 + 50;
         let height = Math.random() * 50 + 50;
         let rectangle = new Rectangle(x, y, speedX, speedY, score, color, width, height);
         graphics.push(rectangle);
-        rectangle.draw(this.ctx);
-        rectangle.move(this.ctx, this.canvas);
+        Background.totalGraphics++;
     }
 
     gameStart() {
         this.game = setInterval(() => {
             this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-            this.addNewGraphics();
             graphics.forEach((item) => {
-                item.draw(this.ctx);
-            });
+                item.draw(this.ctx, this.canvas);
+            })
             this.ctx.font = '30px Arial';
             this.ctx.fillText(`分数：${this.totalScore}`, 10, 50);
             if (lost >= this.max) {
                 clearInterval(this.game);
-                graphics.forEach((item) => {
-                    item.stop();
-                })
+                clearInterval(this.generateInterval);
                 this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
                 graphics = [];
                 this.ctx.fillText('游戏结束', this.canvas.width / 2 - 50, this.canvas.height / 2);
                 this.ctx.fillText('再来一次', this.canvas.width / 2 - 50, this.canvas.height / 2 + 50);
-                this.canvas.addEventListener('click', (e) => {
-                    let x = e.offsetX;
-                    let y = e.offsetY;
-                    if (x > this.canvas.width / 2 - 50 && x < this.canvas.width / 2 + 50 && y > this.canvas.height / 2 && y < this.canvas.height / 2 + 50) {
-                        this.canvas.removeEventListener('click', () => { });
-                        this.tryAgain();
-                    }
+                this.canvas.addEventListener('click', () => {
+                    this.canvas.removeEventListener('click', () => { });
+                    this.tryAgain();
                 });
             }
+        }, 1000 / 60);
+        this.generateInterval = setInterval(() => {
+            Background.totalGraphics < 10 && this.addNewGraphics();
         }, 1000);
     }
 
     tryAgain() {
+        Background.totalGraphics = 0;
         this.totalScore = 0;
         lost = 0;
         this.gameStart();
@@ -88,7 +85,6 @@ class BaseGraphics {
 }
 
 class Rectangle extends BaseGraphics {
-    innerval = null;
 
     constructor(x, y, speedX, speedY, score, color, width, height) {
         super(x, y, speedX, speedY, score, color);
@@ -96,45 +92,30 @@ class Rectangle extends BaseGraphics {
         this.height = height;
     }
 
-    draw(ctx) {
+    draw(ctx, canvas) {
+        this.x += this.speedX;
+        this.y += this.speedY;
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-
-    move(ctx, canvas) {
-        this.innerval = setInterval(() => {
-            ctx.clearRect(this.x, this.y, this.width, this.height);
-            this.x += this.speedX;
-            this.y += this.speedY;
-            this.draw(ctx);
-            if (this.x <= 0 || this.x >= canvas.width - this.width) {
-                clearInterval(this.innerval);
-                ctx.clearRect(this.x, this.y, this.width, this.height);
-                graphics.splice(graphics.indexOf(this), 1);
-                lost++;
-            }
-            if (this.y <= 0 || this.y >= canvas.height - this.height) {
-                clearInterval(this.innerval);
-                ctx.clearRect(this.x, this.y, this.width, this.height);
-                graphics.splice(graphics.indexOf(this), 1);
-                lost++;
-            }
-
-        }, 1000 / 100);
-    }
-
-    isShooted(x, y,ctx) {
-        if (x > this.x && x < this.x + this.width && y > this.y && y < this.y + this.height) {
-            //清除计时器并清除自己
+        if (this.x <= 0 || this.x >= canvas.width - this.width) {
+            this.speedX = -this.speedX;
+        }
+        if (this.y <= 0 || this.y >= canvas.height - this.height) {
             clearInterval(this.innerval);
-            ctx.clearRect(this.x, this.y, this.width, this.height);
+            ctx.clearRect(this.x - 5, this.y - 5, this.width + 5, this.height + 5);
+            graphics.splice(graphics.indexOf(this), 1);
+            lost++;
+            Background.totalGraphics--;
+        }
+    }
+
+    isShooted(x, y, ctx) {
+        if (x > this.x && x < this.x + this.width && y > this.y && y < this.y + this.height) {
+            Background.totalGraphics--;
+            ctx.clearRect(this.x - 5, this.y - 5, this.width + 5, this.height + 5);
             return true;
         }
         return false;
-    }
-
-    stop() {
-        clearInterval(this.innerval);
     }
 }
 
